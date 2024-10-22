0ed1c000dd9cb16507294c0e4206bcb71ac44b6e
tree <tree@hidden.cz>
2024-10-22 15:59:47 +0200

    use builder bundle in js module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import { loadSource } from "./lib/glossary.js";
+//import { loadSource } from "./lib/glossary.js";
+import bundle from './dist/bundle.json';
 
 export function loadGlossary() {
-    return loadSource()
+    return bundle
 }
