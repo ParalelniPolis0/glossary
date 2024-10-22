f539eb2299d16a7e83427e9c9f30bacb550dbcd1
tree <tree@hidden.cz>
2024-10-22 16:57:38 +0200

    add type

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //import { loadSource } from "./lib/glossary.js";
-import bundle from './dist/bundle.json';
+import bundle from './dist/bundle.json' with { type: "json" };
 
 export function loadGlossary() {
     return bundle
